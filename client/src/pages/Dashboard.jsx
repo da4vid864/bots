3f5c759e900b909a91352ed12c231712017e1502
david alvarez tovar <da4avid64@gmail.com>
2025-12-10 23:45:56 -0600

    fix icons on dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -8,118 +8,33 @@ import ScoringRulesManager from '../components/ScoringRulesManager';
 import ProductManager from '../components/ProductManager';
 import axios from 'axios';
 import { 
-    AddIcon, CloseIcon, CheckCircleIcon, WarningIcon, InfoIcon, ErrorIcon,
-    SmartToyIcon, FlashOnIcon, DiamondIcon, ExpandMoreIcon, ExpandLessIcon,
-    StarsIcon, InventoryIcon, AutoAwesomeIcon
+  AddIcon, CloseIcon, CheckCircleIcon, WarningIcon, InfoIcon, ErrorIcon,
+  SmartToyIcon, FlashOnIcon, DiamondIcon, ExpandMoreIcon, ExpandLessIcon,
+  StarsIcon, InventoryIcon
 } from '../components/Icons';
 
-// Iconos SVG embebidos (compatibles con el login)
-const AddIcon = () => (
-  <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
-    <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/>
-  </svg>
-);
-
-const CloseIcon = () => (
-  <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 24 24">
-    <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
-  </svg>
-);
-
-const CheckCircleIcon = () => (
-  <svg className="w-5 h-5 text-green-400" fill="currentColor" viewBox="0 0 24 24">
-    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/>
-  </svg>
-);
-
-const WarningIcon = () => (
-  <svg className="w-5 h-5 text-amber-400" fill="currentColor" viewBox="0 0 24 24">
-    <path d="M1 21h22L12 2 1 21zm12-3h-2v-2h2v2zm0-4h-2v-4h2v4z"/>
-  </svg>
-);
-
-const InfoIcon = () => (
-  <svg className="w-5 h-5 text-blue-400" fill="currentColor" viewBox="0 0 24 24">
-    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z"/>
-  </svg>
-);
-
-const ErrorIcon = () => (
-  <svg className="w-5 h-5 text-red-400" fill="currentColor" viewBox="0 0 24 24">
-    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z"/>
-  </svg>
-);
-
-const SmartToyIcon = () => (
-  <svg className="w-6 h-6 text-blue-400" fill="currentColor" viewBox="0 0 24 24">
-    <path d="M20 9V7c0-1.1-.9-2-2-2h-3c0-1.66-1.34-3-3-3S9 3.34 9 5H6c-1.1 0-2 .9-2 2v2c-1.66 0-3 1.34-3 3s1.34 3 3 3v4c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2v-4c1.66 0 3-1.34 3-3s-1.34-3-3-3zM7.5 11.5c0-.83.67-1.5 1.5-1.5s1.5.67 1.5 1.5S9.83 13 9 13s-1.5-.67-1.5-1.5zM16 17H8v-2h8v2zm-1-4c-.83 0-1.5-.67-1.5-1.5S14.17 10 15 10s1.5.67 1.5 1.5S15.83 13 15 13z"/>
-  </svg>
-);
-
-const FlashOnIcon = () => (
-  <svg className="w-6 h-6 text-green-400" fill="currentColor" viewBox="0 0 24 24">
-    <path d="M7 2v11h3v9l7-12h-4l4-8z"/>
-  </svg>
-);
-
-const DiamondIcon = () => (
-  <svg className="w-6 h-6 text-purple-400" fill="currentColor" viewBox="0 0 24 24">
-    <path d="M12 2L5 12l7 10 7-10z"/>
-  </svg>
-);
-
-const ExpandMoreIcon = () => (
-  <svg className="w-5 h-5 text-slate-400" fill="currentColor" viewBox="0 0 24 24">
-    <path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"/>
-  </svg>
-);
-
-const ExpandLessIcon = () => (
-  <svg className="w-5 h-5 text-slate-400" fill="currentColor" viewBox="0 0 24 24">
-    <path d="M12 8l-6 6 1.41 1.41L12 10.83l4.59 4.58L18 14z"/>
-  </svg>
-);
-
-const StarsIcon = () => (
-  <svg className="w-5 h-5 text-blue-400" fill="currentColor" viewBox="0 0 24 24">
-    <path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zm4.24 16L12 15.45 7.77 18l1.12-4.81-3.73-3.23 4.92-.42L12 5l1.92 4.53 4.92.42-3.73 3.23L16.23 18z"/>
-  </svg>
-);
-
-const InventoryIcon = () => (
-  <svg className="w-5 h-5 text-blue-400" fill="currentColor" viewBox="0 0 24 24">
-    <path d="M20 2H4c-1 0-2 .9-2 2v3.01c0 .72.43 1.34 1 1.69V20c0 1.1 1.1 2 2 2h14c.9 0 2-.9 2-2V8.7c.57-.35 1-.97 1-1.69V4c0-1.1-1-2-2-2zm-5 12H9v-2h6v2zm5-7H4V4h16v3z"/>
-  </svg>
-);
-
-const ArrowRightIcon = () => (
-  <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
-    <path d="M18 6h-2c0-2.21-1.79-4-4-4S8 3.79 8 6H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm-6-2c0-1.1-.9-2-2-2s-2 .9-2 2 0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 2 .9 2 2 2 .9 2 2-.9 2-2 2zm6 10h-12V8h12v10z"/>
-  </svg>
-);
-
-// Nuevos iconos para métricas
+// Iconos adicionales para métricas
 const UsersIcon = () => (
   <svg className="w-6 h-6 text-blue-400" fill="currentColor" viewBox="0 0 24 24">
-  <path d="M16 11c1.66 0 2.99-1.34 2.99-3S17.66 5 16 5c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3S8 5.34 8 7s1.34 3 3 3z"/>
+    <path d="M16 11c1.66 0 2.99-1.34 2.99-3S17.66 5 16 5c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3S8 5.34 8 7s1.34 3 3 3z"/>
   </svg>
 );
 
 const ChartIcon = () => (
   <svg className="w-6 h-6 text-purple-400" fill="currentColor" viewBox="0 0 24 24">
-  <path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zM9 17H7v-5h2v5zm4 0h-2v-3h2v3zm0-6h-2v2h2v-2zm-4 6h-2V7h2v10z"/>
+    <path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zM9 17H7v-5h2v5zm4 0h-2v-3h2v3zm0-6h-2v2h2v-2zm-4 6h-2V7h2v10z"/>
   </svg>
 );
 
 const WalletIcon = () => (
   <svg className="w-6 h-6 text-amber-400" fill="currentColor" viewBox="0 0 24 24">
-  <path d="M18 6h-2c0-2.21-1.79-4-4-4S8 3.79 8 6H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm-6-2c0-1.1-.9-2-2-2s-2 .9-2 2 0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 2 .9 2 2 2 .9 2 2-.9 2-2 2zm6 10h-12V8h12v10z"/>
+    <path d="M18 6h-2c0-2.21-1.79-4-4-4S8 3.79 8 6H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm-6-2c0-1.1-.9-2-2-2s-2 .9-2 2 0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 2 .9 2 2 2 .9 2 2-.9 2-2 2zm6 10h-12V8h12v10z"/>
   </svg>
 );
 
 const MessageIcon = () => (
   <svg className="w-6 h-6 text-green-400" fill="currentColor" viewBox="0 0 24 24">
-  <path d="M20 2H4c-1.1 0-1.99.9-1.99 2L2 22l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 14H6l-2 2V4h16v12zm-4.5-1.1L12 15.45 7.77 18l1.12-4.81-3.73-3.23 4.92-.42L12 5l1.92 4.53 4.92.42-3.73 3.23L16.23 18z"/>
+    <path d="M20 2H4c-1.1 0-1.99.9-1.99 2L2 22l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 14H6l-2 2V4h16v12zm-4.5-1.1L12 15.45 7.77 18l1.12-4.81-3.73-3.23 4.92-.42L12 5l1.92 4.53 4.92.42-3.73 3.23L16.23 18z"/>
   </svg>
 );
 
